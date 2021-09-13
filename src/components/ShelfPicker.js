fd0ac1939c710a7222d9be6ad36af4775143af62
rabobahago <rabobahago@gmail.com>
2021-09-13 03:03:04 +0100

    feat: shelf picker component logic complete

--- src/components/ShelfPicker.js
+++ src/components/ShelfPicker.js
@@ -1,8 +1,27 @@
 import React from "react";
 
 class ShelfPicker extends React.Component {
+  state = {
+    value: this.props.value
+  };
+  handleChange = (e) => {
+    const { book, onUpdateShelves: updateShelves } = this.props;
+    this.setState({ value: e.target.value });
+    updateShelves(book, e.target.value);
+  };
   render() {
-    return <h4>Shelves</h4>;
+    const { book } = this.props;
+    return (
+      <select value={book.shelf} onChange={this.handleChange}>
+        <option value="move" disabled>
+          Move to...
+        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    );
   }
 }
 export default ShelfPicker;
